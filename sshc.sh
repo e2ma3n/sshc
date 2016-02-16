61d160982d449172231b810e1c8fe7ab6ca6e9e4
Iman Homayouni <e2ma3n@gmail.com>
2016-02-16 20:52:59 +0330

    Update and rename sshc to sshc.sh

--- sshc.sh
+++ sshc.sh
@@ -1,34 +1,31 @@
 #! /bin/bash
 # Programming and idea by : E2MA3N [Iman Homayouni]
+# Gitbub : https://github.com/e2ma3n
 # Email : e2ma3n@Gmail.com
 # Website : http://OSLearn.ir
 # License : GPL v3.0
-# sshc v3.0 - core [SSH Management Console]
+# sshc v4.0 - core [SSH Management Console]
 #--------------------------------------------------------#
 
 # check root privilege
-if [ "`whoami`" != "root" ]; then
-        echo -e '[-] Please use root user or sudo'
-        exit 1
-fi
+[ "`whoami`" != "root" ] && echo -e '[-] Please use root user or sudo' && exit 1
 
 
 # data base location , Don not change this form
-database_en="/etc/sshc/sshc.database.en"
+database_en="/opt/sshc_v4/sshc.database.en"
 
 
-# echo header on terminal
+# print header on terminal
 reset
 echo '[+] ------------------------------------------------------------------- [+]'
 echo -e "[+] Programming and idea by : \e[1mE2MA3N [Iman Homayouni]\e[0m"
 echo '[+] License : GPL v3.0'
-echo -e '[+] sshc v3.0 \n'
+echo -e '[+] sshc v4.0 \n'
 
 
 # check encrypted database
 if [ ! -f $database_en ] ; then
-	echo -e "[-] Error:"
-	echo -e "[-] $database_en not found"
+	echo -e "[-] Error: $database_en not found"
 	echo '[+] ------------------------------------------------------------------- [+]'
 	exit 1
 fi
@@ -38,7 +35,7 @@ fi
 echo -en "[+] Enter password: " ; read -s pass
 database_de=`openssl aes-256-cbc -pass pass:$pass -d -a -in $database_en 2> /dev/null`
 if [ "$?" != "0" ] ; then
-	echo -e "\n[-] Error: Database can not encrypted."
+	echo -e "\n[-] Error: Database can not decrypted."
 	echo '[+] ------------------------------------------------------------------- [+]'
 	exit 1
 else
@@ -49,18 +46,19 @@ fi
 # print servers informations on terminal
 echo -e "\n 0) Edite Database"
 var0=`echo "$database_de" | wc -l`
-var0=`expr $var0 - 11`
+var0=`expr $var0 - 12`
 for (( i=1 ; i <= $var0 ; i++ )) ; do
-echo -ne " $i) " ; echo "$database_de" | tail -n $i | head -n 1 | cut -d " " -f 1,2 | tr " " @
+	echo -ne " $i) " ; echo "$database_de" | tail -n $i | head -n 1 | cut -d " " -f 1,2 | tr " " @
 done
 
 
 # edite database
 function edit_db {
-	echo "$database_de" | joe /etc/sshc/sshc.database.de
+	echo "$database_de" > /opt/sshc_v4/sshc.database.de
+	nano /opt/sshc_v4/sshc.database.de
 	echo -en "[+] encrypt new database, Please type your password: " ; read -s pass
-	openssl aes-256-cbc -pass pass:$pass -a -salt -in /etc/sshc/sshc.database.de -out $database_en
-	rm -f /etc/sshc/sshc.database.de
+	openssl aes-256-cbc -pass pass:$pass -a -salt -in /opt/sshc_v4/sshc.database.de -out $database_en
+	rm -f /opt/sshc_v4/sshc.database.de &> /dev/null
 	echo -e "\n[+] Done, New database saved and encrypted"
 	echo '[+] ------------------------------------------------------------------- [+]'
 	exit 0
@@ -69,23 +67,23 @@ function edit_db {
 
 # select server for continue
 while :; do
-echo -en '\e[0m\n[+] Select your server/option or type quit for exit: ' ; read var1
+	echo -en '\e[0m\n[+] Select your server/option or type quit for exit: ' ; read var1
 
-if [ "$var1" = "0" ] ; then
-	edit_db
-fi
+	if [ "$var1" = "0" ] ; then
+		edit_db
+	fi
 
-if [ "$var1" -le "$var0" ] 2> /dev/null ; then
-	break
-elif [ "$var1" = "quit" ] ; then
-	echo "[+] Bye Bye"
-	echo '[+] ------------------------------------------------------------------- [+]'
-	exit 1
-else
-	echo "[-] Error: bad input"
-	echo '[+] ------------------------------------------------------------------- [+]'
-	exit 1
-fi
+	if [ "$var1" -le "$var0" ] 2> /dev/null ; then
+		break
+	elif [ "$var1" = "quit" ] ; then
+		echo "[+] Bye Bye"
+		echo '[+] ------------------------------------------------------------------- [+]'
+		exit 1
+	else
+		echo "[-] Error: bad input"
+		echo '[+] ------------------------------------------------------------------- [+]'
+		exit 1
+	fi
 done
 
 
@@ -103,30 +101,37 @@ function function_1 {
 # add your ip address to firewall
 function function_2 {
 	if [ "`echo "$database_de" | tail -n $var1 | head -n 1 | cut -d " " -f 1`" != "root" ] ; then
-	echo "[-] Error: Your user is not root, we can not add your ip to firewall"
-	echo '[+] ------------------------------------------------------------------- [+]'
-	exit 1
+		echo "[-] Error: Your user is not root, we can not add your ip to firewall"
+		echo '[+] ------------------------------------------------------------------- [+]'
+		exit 1
 	fi
+	
 	password=`echo "$database_de" | tail -n $var1 | head -n 1 | cut -d " " -f 4`
 	username=`echo "$database_de" | tail -n $var1 | head -n 1 | cut -d " " -f 1`
 	ssh_port=`echo "$database_de" | tail -n $var1 | head -n 1 | cut -d " " -f 3`
 	ip_address=`echo "$database_de" | tail -n $var1 | head -n 1 | cut -d " " -f 2`
+	
 	for (( i=1 ; i < 4 ; i++ )) ; do
 		public_ip=`curl ipecho.net/plain 2> /dev/null`
 		if [ ! -z "$public_ip" ]; then
 			break
 		fi
 	done
-	echo -en "[+] $public_ip your public IP Address ? [y/n]: " ; read var4
-	if [ "$var4" = "y" ] ; then
+	
+	echo -en "[+] $public_ip your public IP Address ? [y/n]: " ; read var3
+	if [ "$var3" = "y" ] ; then
 		sshpass -p "$password" ssh -o StrictHostKeyChecking=no -l $username $ip_address -p $ssh_port "iptables -I INPUT -s $public_ip -j ACCEPT" &> /dev/null
 		if [ "$?" != "0" ] ; then
 			echo "[-] Error: Can not connect to server."
 			echo '[+] ------------------------------------------------------------------- [+]'
 			exit 1
 		fi
+	elif [ "$var3" = "n" ] ; then
+		echo '[-] Are you shore ? Please try again'
+		echo '[+] ------------------------------------------------------------------- [+]'
+		exit 1
 	else
-		echo "[+] Bye Bye"
+		echo "[-] Error: Bad input"
 		echo '[+] ------------------------------------------------------------------- [+]'
 		exit 1
 	fi
