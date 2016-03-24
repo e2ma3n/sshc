cf999002450843880ede3405f566000b658e168b
Iman Homayouni <e2ma3n@gmail.com>
2016-03-24 08:07:21 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # Email : e2ma3n@Gmail.com
 # Website : http://OSLearn.ir
 # License : GPL v3.0
-# sshc v4.0 - installer [SSH Management Console]
+# sshc v5.0 - installer [SSH Management Console]
 #--------------------------------------------------------#
 
 # check root privilege
@@ -15,27 +15,39 @@
 function help_f {
 	echo "Usage: "
 	echo "	sudo ./install.sh -i [install program]"
+	echo '	sudo ./install.sh -u [help to uninstall program]'
 	echo "	sudo ./install.sh -c [check dependencies]"
 }
 
 
 # install program on system
 function install_f {
-	[ ! -d /opt/sshc_v4/ ] && mkdir -p /opt/sshc_v4/ && echo "[+] Directory created" || echo "[-] Error: /opt/sshc_v4/ exist"
+	[ ! -d /opt/sshc_v5/ ] && mkdir -p /opt/sshc_v5/ && echo "[+] Directory created" || echo "[-] Error: /opt/sshc_v5/ exist"
 	sleep 1
-	[ ! -f /opt/sshc_v4/sshc.sh ] && cp sshc.sh /opt/sshc_v4/ && chmod 755 /opt/sshc_v4/sshc.sh && echo "[+] sshc.sh copied" || echo "[-] Error: /opt/sshc_v4/sshc.sh exist"
+	[ ! -f /opt/sshc_v5/sshc.sh ] && cp sshc.sh /opt/sshc_v5/ && chmod 755 /opt/sshc_v5/sshc.sh && echo "[+] sshc.sh copied" || echo "[-] Error: /opt/sshc_v5/sshc.sh exist"
 	sleep 1
-	[ ! -f /opt/sshc_v4/sshc.database.en ] && cp sshc.database.en /opt/sshc_v4/sshc.database.en && chown root:root /opt/sshc_v4/sshc.database.en && chmod 700 /opt/sshc_v4/sshc.database.en && echo "[+] sshc.database.en copied" || echo "[-] Error: /opt/sshc_v4/sshc.database.en exist"
+	[ ! -f /opt/sshc_v5/sshc.database.en ] && cp sshc.database.en /opt/sshc_v5/sshc.database.en && chown root:root /opt/sshc_v5/sshc.database.en && chmod 700 /opt/sshc_v5/sshc.database.en && echo "[+] sshc.database.en copied" || echo "[-] Error: /opt/sshc_v5/sshc.database.en exist"
 	sleep 1
-	[ -f /opt/sshc_v4/sshc.sh ] && ln -s /opt/sshc_v4/sshc.sh /usr/bin/sshc && echo "[+] symbolic link created" || echo "[-] Error: symbolic link not created"
+	[ -f /opt/sshc_v5/sshc.sh ] && ln -s /opt/sshc_v5/sshc.sh /usr/bin/sshc && echo "[+] symbolic link created" || echo "[-] Error: symbolic link not created"
 	sleep 1
-	[ ! -f /opt/sshc_v4/README ] && cp README /opt/sshc_v4/README && chmod 644 /opt/sshc_v4/README && echo "[+] README copied" || echo "[-] Error: /opt/sshc_v4/README exist"
+	[ ! -f /opt/sshc_v5/README ] && cp README /opt/sshc_v5/README && chmod 644 /opt/sshc_v5/README && echo "[+] README copied" || echo "[-] Error: /opt/sshc_v5/README exist"
 	sleep 1
 
 	echo "[+] Please see README"
+	sleep 0.5
 	echo "[!] Warning: run program and edit your database."
+	sleep 0.5
 	echo "[!] Warning: defaul password is 'sshc'"
+	sleep 0.5
 	echo "[+] Done"
+}
+
+
+# uninstall program from system
+function uninstall_f {
+	echo 'For uninstall program:'
+	echo '	sudo rm -rf /opt/sshc_v5/'
+	echo '	sudo rm -f /usr/bin/sshc'
 }
 
 
@@ -49,6 +61,7 @@ function check_f {
 		else
 			echo -e "[-] Error: $program not found"
 		fi
+		sleep 0.5
 	done
 }
 
@@ -56,5 +69,6 @@ function check_f {
 case $1 in
 	-i) install_f ;;
 	-c) check_f ;;
+	-u) uninstall_f ;;
 	*) help_f ;;
 esac
